a225f4ca9b307a27f60a23a57c90fb1a7fc728de
Matt Casper <matthewvcasper@gmail.com>
2015-02-23 20:41:15 -0800

    filtered_ls

--- nodejs/filtered_ls.js
+++ nodejs/filtered_ls.js
@@ -1,17 +1,12 @@
 var dir = process.argv[2]
-var extension = process.argv[3]
+var extension = "." + process.argv[3]
 var fs = require("fs")
+var path = require("path")
 
-function printIf(file, index, arr) {
-  if (file.extension == extension) {
-    console.log("hi")
-  }
-}
-
-var what = fs.readdir(dir, "utf8", function doneReading(err, list) {
-  console.log("before the forEach")
-  list.forEach(printIf)
-  console.log("after the forEach")
+fs.readdir(dir, function callback (err, list) {
+  list.forEach(function(file) {
+    if (path.extname(file) == extension) {
+      console.log(file)
+    }
+  })
 })
-
-what
